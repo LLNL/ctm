074477b85cfb33794ebf056393ff6a45d0371f13
Ignacio Aravena <aravenasolis1@llnl.gov>
2024-08-08 12:06:44 -0700

    adding example to get data as vector in c++

--- examples/scripts/cpp_example/parse_and_time.cpp
+++ examples/scripts/cpp_example/parse_and_time.cpp
@@ -5,6 +5,8 @@
 #include <string>   // necessary for passing file names
 #include <fstream>  // necessary for handling from files
 #include <iostream> // necessary for printing to screen
+#include <vector>   // necessary for handling vectors
+#include <functional> // necessayr for using std::function
 
 #include "timer.hpp"
 #include "ctm_schemas.hpp"
@@ -18,6 +20,43 @@ auto parse_json_file(const std::string& fname)
 using ctm_schemas::CtmData;
 using ctm_schemas::CtmSolution;
 
+std::string uid_to_string(const ctm_schemas::BusFr& uid)
+{
+  if(std::holds_alternative<int64_t>(uid))
+  {
+    return std::to_string(std::get<int64_t>(uid));
+  }else{
+    return std::get<std::string>(uid);
+  }
+}
+
+template<typename T>
+std::string get_uid(const T& obj) { return uid_to_string(obj.uid); }
+
+template<typename U, typename T>
+std::vector<U> get_member_vector(const std::vector<T>& vec,
+                                 std::function<U(const T&)> get_member_fun)
+{
+  std::vector<U> out;
+  out.reserve(vec.size());
+  for(const T& elem: vec)
+  {
+    out.push_back(get_member_fun(elem));
+  }
+  return out;
+}
+
+template<typename T>
+void print_vector(const std::vector<T>& vec)
+{
+  std::cout << "[";
+  for(const T& elem: vec)
+  {
+    std::cout << " " << elem;
+  }
+  std::cout << " ]";
+}
+
 int main()
 {
   Timer clock;
@@ -27,6 +66,12 @@ int main()
   clock.toc();
   std::cout << "Read UC data file in " << clock.duration().count() << " milliseconds." << std::endl;
   std::cout << "System has " << sys.network.bus.size() << " buses." << std::endl; 
+  auto uid_buses = get_member_vector<std::string,
+                                     ctm_schemas::NetworkBus>(sys.network.bus,
+                                                              get_uid<ctm_schemas::NetworkBus>); 
+  std::cout << "Bus uids: ";
+  print_vector(uid_buses);
+  std::cout << std::endl;
   
   clock.tic();
   CtmSolution sol = parse_json_file("../../instances/unit_commitment_solutions/2020-01-27_solution.json");
